a5a9af2719e943d5a58e6dd816c3f9abb9a4a12f
Joshua-CY <101m02003@stud.sju.edu.tw>
2023-03-09 10:24:00 +0800

    init

---
+++ src/views/DetailView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="detail">
+    <h1>This is an detail page</h1>
+    <h2>ref & setup 基本使用方法</h2>
+    <h2>{{ counter }}</h2>
+    <button @click="add">+</button>
+    <button @click="minus">-</button>
+  </div>
+  <Footer></Footer>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .detail {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import Footer from '@/views/components/Footer.vue';
+
+const counter = ref(0)
+
+const add = (): void => {
+      counter.value = counter.value + 1
+    }
+
+const minus = (): void => {
+  counter.value = counter.value - 1
+}
+
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+import Footer from '@/views/components/Footer.vue';
+</script>
+
+<template>
+  <main>
+    <!-- <TheWelcome /> -->
+    <div class="home">
+      <h1>This is an home page</h1>
+    </div>
+    <Footer></Footer>
+  </main>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .home {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
