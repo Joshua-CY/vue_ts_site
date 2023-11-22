1d33bcc5585bc33b5226e13c7d2af6c19aa3dd19
Joshua-CY <101m02003@stud.sju.edu.tw>
2023-11-22 16:37:11 +0800

    增加註解

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,6 +1,7 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
+// 使用composition API模式定義store
 export const useCounterStore = defineStore('counter', () => {
   const count = ref(0)
   const doubleCount = computed(() => count.value * 2)
--- src/stores/useCounterStore.ts
+++ src/stores/useCounterStore.ts
@@ -1,5 +1,6 @@
 import { defineStore } from 'pinia';
 
+// 使用options API模式定義
 export const useCounterStoreV2 = defineStore('counter', {
     state: () => {
         return { count: 0 }
